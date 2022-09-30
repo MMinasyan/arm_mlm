a2d95b5bd75143bf777a71988f4ae041a5fa34c4
Mkrtich Minasyan <mvminasyan.96@gmail.com>
2022-09-30 17:06:41 +0400

    created load_dicts() in utils.py

---
+++ utils.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+
+def load_dicts():
+    if 'char2idx_latarm.json' not in os.listdir('data/') and 'idx2char_latarm.json' not in os.listdir('data/'):
+        armchars = 'աբգդեզէըթժիլխծկհձղճմյնշոչպջռսվտրցւփքևօֆ'
+        latinchars = 'abcdefghijklmnopqrstuvwxyz'
+        otherchars = '1234567890,./;\'\[\]\\-=`~!@#$%^&*()_+|{}:\"<>?«»,․/՞՝՜;՛։֊―\n '
+        allchars = list(set(list(armchars) + list(armchars.upper()) + list(latinchars) + list(latinchars.upper()) + list(otherchars)))
+
+        char2idx = {'oov': 1}
+        for i in range(2, len(allchars)+2):
+            char2idx[allchars[i-2]] = i
+        char2idx['MASK'] = len(char2idx)+1
+        idx2char = {}
+        for k in char2idx.keys():
+            idx2char[char2idx[k]] = k
+
+        with open('data/char2idx_latarm.json', mode='w', encoding='utf-8') as fp:
+            json.dump(char2idx, fp)
+        with open('data/idx2char_latarm.json', mode='w', encoding='utf-8') as fp:
+            json.dump(idx2char, fp)
+        print('files not existing. created new files.')
+    
+    with open('data/char2idx_latarm.json', mode='r', encoding='utf-8') as file:
+        char2idx = json.load(file)
+    with open('data/idx2char_latarm.json', mode='r', encoding='utf-8') as file:
+        idx2char = json.load(file)
+    return char2idx, idx2char
