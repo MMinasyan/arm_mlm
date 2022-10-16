d56efd1d665dba1634e6373fece999c013b95095
Mkrtich Minasyan <mvminasyan.96@gmail.com>
2022-10-16 19:13:52 +0400

    created ModelConfigurator class in modeling

---
+++ modeling.py
@@ -0,0 +1,7 @@
+class ModelConfigurator():
+    def __init__(self, MAX_LEN, NUM_HEAD, FF_DIM, NUM_LAYERS, EMBED_DIM):
+        self.MAX_LEN = MAX_LEN
+        self.NUM_HEAD = NUM_HEAD
+        self.FF_DIM = FF_DIM
+        self.NUM_LAYERS = NUM_LAYERS
+        self.EMBED_DIM = EMBED_DIM
